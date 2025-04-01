45085033de138b7f772236809025a717105eaaf3
mrhl082024 <mrhl082024@gmail.com>
2025-04-01 11:06:40 +0200

    hero,footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="relative ">
+    <footer className="relative w-[100%] bg-green-950 border-t-4 border-emerald-700 border-double">
       <div>Footer test</div>
     </footer>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
 export default function Hero() {
   return (
-    <section className="h-[100svh] bg-[url(../assets/fanart/1133045.jpg)]">
-      <h1>Welcome to Hyrule Compendium!</h1>
-      <p>
-        Hyrule Compendium mimics the photo album from Zelda Breath of the Wild.{" "}
+    <section className="h-[100svh] bg-[url(../assets/fanart/1133045.jpg)] flex justify-center">
+      <div className="bg-green-900/80 w-[600px] h-[fit-content] rounded-[6px] p-[18px] mt-[2rem]">
+        <h1>Welcome to Hyrule Compendium!</h1>
+        <p>
+          Hyrule Compendium mimics the photo album from Zelda Breath of the
+          Wild. <br />
+          Click on a category to get started!
+        </p>
         <br />
-        Click on a category to get started!
-      </p>
-      <br />
-      <p>
-        The goal of this project was to take the first step towards learning
-        Next.js and Tailwindcss. Expanding my knowlegde and skillset within
-        frontend development.
-      </p>
+        <p>
+          The goal of this project was to take the first step towards learning
+          Next.js and Tailwindcss. Expanding my knowlegde and skillset within
+          frontend development.
+        </p>
+      </div>
     </section>
   );
 }
