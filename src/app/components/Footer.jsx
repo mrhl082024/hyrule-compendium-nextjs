974123da6db7e3f4af32d167e437247fe1378556
mrhl082024 <mrhl082024@gmail.com>
2025-04-02 23:19:43 +0100

    ADD footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer className="relative w-[100%] bg-green-950 border-t-4 border-emerald-700 border-double">
-      <div>Footer test</div>
+      <p>This was made using Next.js and Tailwindcss.</p>
+      <a href="">
+        My Github: <img src="" alt="" />
+      </a>
     </footer>
   );
 }
