839920147bcc835c5cafaaa85fa83dd49c6c3150
mrhl082024 <mrhl082024@gmail.com>
2025-04-02 23:25:11 +0100

    ADD footer, logo img

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,14 @@
+import githubLogo from "../../assets/logos/github-mark-white.png";
 export default function Footer() {
   return (
-    <footer className="relative w-[100%] bg-green-950 border-t-4 border-emerald-700 border-double">
+    <footer className="relative w-[100%] bg-green-950 border-t-4 border-emerald-700 border-double flex items-center flex-col p-[20px]">
       <p>This was made using Next.js and Tailwindcss.</p>
-      <a href="">
-        My Github: <img src="" alt="" />
+      <a href="https://github.com/mrhl082024" target="_blank">
+        <img
+          src={githubLogo.src}
+          alt="github logo"
+          className="w-[30px] h-[30px]"
+        />
       </a>
     </footer>
   );
